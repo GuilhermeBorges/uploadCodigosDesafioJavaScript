ef06babb53a1d903e3b11e8e4b6492d56618ae78
GuilhermeBorges <guilhermeborgesoliveira@gmail.com>
2020-02-28 18:35:03 -0300

    Update Area do Circulo.js
    tem este bagúio chamado template string que a gente não usa bastante

--- Area do Circulo.js
+++ Area do Circulo.js
@@ -1,5 +1,6 @@
 var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var lines = input.split('\n');
 
-var raio = parseFloat(lines)
-console.log("A=" + (3.14159 * Math.pow(raio,2)).toFixed(4))
+const raio = parseFloat(lines)
+const area = (3.14159 * Math.pow(raio,2)).toFixed(4)
+console.log(`A=${area}`)
