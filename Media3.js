af5ed87f584ac8f8c11fdddcf7989046d48e5f2f
GuilhermeBorges <guilhermeborgesoliveira@gmail.com>
2020-02-28 19:24:05 -0300

    Update Media3.js
    Encontramos dois erros textuais (um 'F' maiúsculo e um 'repovado' que deveria ser 'reprovado') que faziam com que o URI não funcionasse. Parece que você não testou direito (ou não prestou atenção), mas foda-se, funciona :)

--- Media3.js
+++ Media3.js
@@ -4,30 +4,30 @@ var lines = input.split('\n');
 var exame = parseFloat(lines[1])
 var notas = lines[0]
 notas = notas.split(' ')
-var nota1 = parseFloat(notas[0]*2)
-var nota2 = parseFloat(notas[1]*3)
-var nota3 = parseFloat(notas[2]*4)
+var nota1 = parseFloat(notas[0] * 2)
+var nota2 = parseFloat(notas[1] * 3)
+var nota3 = parseFloat(notas[2] * 4)
 var nota4 = parseFloat(notas[3])
-var media = (nota1 + nota2 + nota3 + nota4)/10
+var media = (nota1 + nota2 + nota3 + nota4) / 10
 
 console.log('Media: ' + media.toFixed(1))
-if (exame >= 0){    
-    console.log('Aluno em exame.')
-    console.log('Nota do exame: ' + exame.toFixed(1))
-    var mediaF = (media + exame)/2
-    if (mediaF >= 5){
-        console.log('Aluno aprovado.')        
-    }
-    else{
-        console.log('Aluno reprovado.')
-    }
-    console.log('Media Final: ' + mediaF.toFixed(1))
+if (exame >= 0) {
+  console.log('Aluno em exame.')
+  console.log('Nota do exame: ' + exame.toFixed(1))
+  var mediaF = (media + exame) / 2
+  if (mediaF >= 5) {
+    console.log('Aluno aprovado.')
+  }
+  else {
+    console.log('Aluno reprovado.')
+  }
+  console.log('Media final: ' + mediaF.toFixed(1))
 }
-else{
-    if (media < 7){
-        console.log('Aluno repovado.')
-    }
-    else{        
-        console.log('Aluno aprovado.')
-    }
+else {
+  if (media < 7) {
+    console.log('Aluno reprovado.')
+  }
+  else {
+    console.log('Aluno aprovado.')
+  }
 }
