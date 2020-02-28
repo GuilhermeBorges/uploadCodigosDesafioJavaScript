5d2c9073ceae84beb745a28d722bbfdfd98a18d0
GuilhermeBorges <guilhermeborgesoliveira@gmail.com>
2020-02-28 19:01:14 -0300

    Update Experiencias.js
    As variáveis Qco, Qra, Qsa são muito ruins, difícil de entender sem contar que deveriam ser Pco, seguindo a lógica (já que são porcentagem).
    
    Geralmente, utilizamos variáveis com a primeira em maíuscula somente quanto é uma Classe.
    
    Ao invés de usar vários "if"s você pode utilizar um switch case para ficar mais legível

--- Experiencias.js
+++ Experiencias.js
@@ -2,36 +2,38 @@ var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var lines = input.split('\n');
 
 lines.shift()
-var Coelho = 0
-var Rato = 0
-var Sapo = 0
+var coelho = 0
+var rato = 0
+var sapo = 0
 var sum = 0
 
 lines.forEach(element => {
-    element = element.trim()
-    element = element.split(' ')
-    var quantia = parseInt(element[0])
-    var tipo = element[1]
-    if (tipo === 'C'){
-        Coelho += quantia
-    }
-    else if (tipo === 'R'){
-        Rato += quantia
-    }
-    else if (tipo === 'S'){
-        Sapo += quantia
-    }    
-});
+  element = element.trim()
+  element = element.split(' ')
+  const quantia = parseInt(element[0])
+  const tipo = element[1]
+  switch (tipo) {
+    case 'C': 
+      coelho += quantia
+      break
+    case 'E': 
+      rato += quantia
+      break
+    case 'S': 
+      sapo += quantia
+      break
+  }
+})
 
-sum = Coelho + Rato + Sapo
-    var Qco = (Coelho*100)/sum
-    var Qra = (Rato*100)/sum
-    var Qsa = (Sapo*100)/sum
+sum = coelho + rato + sapo
+const porcentagemCoelhos = (coelho * 100) / sum
+const porcentagemRatos = (rato * 100) / sum
+const porcentagemSapos = (sapo * 100) / sum
 
-console.log('Total: ' + sum + " cobaias");
-console.log('Total de coelhos: ' + Coelho);
-console.log('Total de ratos: ' + Rato);
-console.log('Total de sapos: ' + Sapo);
-console.log('Percentual de coelhos: ' + Qco.toFixed(2) + " %");
-console.log('Percentual de ratos: ' + Qra.toFixed(2) + " %");
-console.log('Percentual de sapos: ' + Qsa.toFixed(2) + " %");
+console.log('Total: ' + sum + " cobaias")
+console.log('Total de coelhos: ' + coelho)
+console.log('Total de ratos: ' + rato)
+console.log('Total de sapos: ' + sapo)
+console.log('Percentual de coelhos: ' + porcentagemCoelhos.toFixed(2) + " %")
+console.log('Percentual de ratos: ' + porcentagemRatos.toFixed(2) + " %")
+console.log('Percentual de sapos: ' + porcentagemSapos.toFixed(2) + " %")
